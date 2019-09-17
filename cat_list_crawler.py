c8192b6b67dc5d3dcd4a6342b3f7edd2c633c7b8
mopmop <mopmopsolucky@gmail.com>
2019-09-17 20:15:20 +0900

    parse all cat detail page

--- cat_list_crawler.py
+++ cat_list_crawler.py
@@ -8,15 +8,20 @@ import re
 import sys
 import spider
 import logging
+import datetime
 import requests
 from bs4 import BeautifulSoup
+
 requests.packages.urllib3.disable_warnings()
+
+search_url = "https://www.petstation.jp/animal_list.php?_search_animal__animal_search__species=2&_search_shop_info__animal_search__shop_address2=9&l1_s="
 
 
 class MyFetcher(spider.Fetcher):
     """
     重写spider.Fetcher类，可自定义初始化函数，这里必须重写父类中的url_fetch函数
     """
+
     def url_fetch(self, priority: int, url: str, keys: dict, deep: int, repeat: int, proxies=None):
         """
         定义抓取函数，注意参见父类中对应函数的参数和返回值说明
@@ -46,12 +51,41 @@ class MyParser(spider.Parser):
         status_code, url_now, html_text = content
         soup = BeautifulSoup(html_text, "html.parser")
         table = soup.table
-        headers = table.find_all("h3")
-        cat_list = []
-        for h in headers:
-            cat_list.append(f'https://www.petstation.jp/{h.a["href"]}')
+        if "animal_list" in url:
+            print("list")
+            headers = table.find_all("h3")
+            url_list = []
+            for h in headers:
+                url_list.append((f'https://www.petstation.jp/{h.a["href"]}', keys, priority + 1))
 
-        return 1, [], cat_list
+            if len(headers) > 0:
+                offset = url[url.rfind('=') + 1:]
+                offset = int(offset) + 10
+                print(offset)
+                url_list.append((f'{search_url}{offset}', keys, priority + 1))
+            print(url_list)
+            return 1, url_list, None
+        else:
+            tds = table.find_all("td")
+            kind = format_text(tds[0].text)
+            price = format_text(tds[1].text)
+            gender = format_text(tds[2].text)
+            color = format_text(tds[3].text)
+            birthday = format_text(tds[4].text)
+            cert = format_text(tds[5].text)
+
+            item = {
+                "url": url,
+                "kind": kind,
+                "price": price,
+                "gender": gender,
+                "color": color,
+                "birthday": birthday,
+                "cert": cert,
+                "datetime": datetime.datetime.now()
+            }
+
+            return 1, [], item
 
 
 class MySaver(spider.Saver):
@@ -71,8 +105,10 @@ class MySaver(spider.Saver):
         """
         定义保存函数，将item保存到本地文件或者数据库
         """
-        self._save_pipe.write("\n".join(item))
-        self._save_pipe.flush()
+        if item is not None:
+            self._save_pipe.write(",".join([str(item[col]) for col in item]))
+            self._save_pipe.write("\n")
+            self._save_pipe.flush()
         return 1, None
 
 
@@ -101,8 +137,8 @@ def test_spider():
     """
     # 初始化 fetcher / parser / saver / proxieser
     fetcher = MyFetcher(sleep_time=0, max_repeat=1)
-    parser = MyParser(max_deep=1)
-    saver = MySaver(save_pipe=open("cat_list.txt", "w"))
+    parser = MyParser(max_deep=10)
+    saver = MySaver(save_pipe=open("cats.txt", "w"))
     # proxieser = MyProxies(sleep_time=5)
 
     # 定义url_filter
@@ -114,11 +150,11 @@ def test_spider():
     # web_spider = spider.WebSpider(fetcher, parser, saver, proxieser=proxieser, url_filter=url_filter, queue_parse_size=100, queue_proxies_size=100)
 
     # 添加起始的url
-    web_spider.set_start_url("https://www.petstation.jp/animal_list.php?_search_animal__animal_search__species=2&_search_shop_info__animal_search__shop_address2=9&l1_s=0", priority=0,
+    web_spider.set_start_url(f'{search_url}0', priority=0,
                              keys={"type": "index"}, deep=0)
 
     # 开启爬虫web_spider
-    web_spider.start_working(fetcher_num=20)
+    web_spider.start_working(fetcher_num=40)
 
     # 等待爬虫结束
     web_spider.wait_for_finished()
